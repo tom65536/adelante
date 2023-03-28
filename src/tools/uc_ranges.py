63308415664b153d55b4f6d4f26903c9a0ca8b59
Thomas Reiter <peregrinus@planet.ms>
2023-03-28 21:08:14 +0000

    adds more productions and tokens

--- src/tools/uc_ranges.py
+++ src/tools/uc_ranges.py
@@ -7,21 +7,21 @@ max_uc = 0xFFFF
 class CharacterClass:
     def includes_categories(self):
         return []
-        
+
     def excludes_categories(self):
         return []
-        
+
     def includes_characters(self):
         return []
-        
+
     def excludes_characters(self):
         return []
-        
+
     def matches_codepoint(self, codepoint):
         return self.matches_character(
             chr(codepoint)
         )
-        
+
     def matches_character(self, character):
         ch_cat = unicodedata.category(
             character
@@ -31,74 +31,88 @@ class CharacterClass:
                 break
         else:
             if (character not in
-                self.includes_characters()
-            ):
+                    self.includes_characters()
+                    ):
                 return False
         for cat in self.excludes_categories():
             if ch_cat.startswith(cat):
                 return False
         return (character not in
-            self.excludes_characters())
-            
+                self.excludes_characters())
+
     def __hash__(self):
         return hash(self.__class__.__name__)
-        
+
     def __eq__(self, other):
         return str(self) == str(other)
-            
+
     def __str__(self):
         return self.__class__.__name__
+
 
 class NoSyntax(CharacterClass):
     def excludes_characters(self):
         return super().excludes_characters() \
-           + ['.', ',', '(', ')',
-              '[', ']', '{', '}',
-              '#', '"', "'", '!',
-              '%', '&', '^', '@',
-              '=', '`', '?', '-',
-              '+', '*', '/', '|',
-              '>', '>', '~', ';']
-                  
+            + ['.', ',', '(', ')',
+               '[', ']', '{', '}',
+               '#', '"', "'", '!',
+               '%', '&', '^', '@',
+               '=', '`', '?', '-',
+               '+', '*', '/', '|',
+               '>', '>', '~', ';']
+
+
 class Whitespace(NoSyntax):
     def includes_categories(self):
         return super().includes_categories() \
-        + ['Zs']
-        
+            + ['Zs']
+
     def includes_characters(self):
         return super().includes_characters() \
-        + ['\t']
-        
+            + ['\t']
+
+
 class Medial(Whitespace):
     def includes_characters(self):
         return super().includes_characters() \
-        + ['_', '\u200C', '\u200D']
-        
+            + ['_', '\u200C', '\u200D']
+
+
 class Start(NoSyntax):
     def includes_categories(self):
         return super().includes_categories() \
-        + ['L', 'Nl', 'Sc']
-        
+            + ['L', 'Nl', 'Sc']
+
+    def excludes_characters(self):
+        return super().excludes_characters() \
+            + ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+               'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
     def includes_characters(self):
         return super().includes_characters() \
-        + ['\\']
-            
+            + ['\\']
+
+
 class Continue(Start):
     def includes_categories(self):
         return super().includes_categories() \
-        + ['Mn', 'Mc', 'N', 'Pc', 'So']
-        
+            + ['Mn', 'Mc', 'N', 'Pc', 'So']
+
     def includes_characters(self):
         return super().includes_characters() \
-        + ['\u0E33', '\u0EB3',
-            '\uFF9E', '\FF9F',
-            '\u05F3']
-                 
+            + ['\u0E33', '\u0EB3',
+               '\uFF9E', '\FF9F',
+               '\u05F3']
+
+
 def escape_cp(codepoint):
     if codepoint == 0x0A:
         return r'"\n"'
+    if codepoint == 0x5C:
+        return r'"\\"'
     if codepoint <= 0xFFFF:
-        return f'"\\u{codepoint:04x}"' 
+        return f'"\\u{codepoint:04x}"'
     return f'"\\U{codepoint:06x}"'
 
 
@@ -106,7 +120,8 @@ def escape_range(a, b):
     if a == b:
         return escape_cp(a)
     return escape_cp(a) + '-' + escape_cp(b)
-    
+
+
 classes = (
     Whitespace(),
     Medial(),
@@ -126,13 +141,13 @@ prev = 0
 for code_point in range(1, max_uc+1):
     for c in classes:
         fit = c.matches_codepoint(code_point)
-    
+
         if range_start[c] and not fit:
             ranges[c].append(
                 (range_start[c], prev))
             range_start[c] = None
         elif fit and not range_start[c]:
-            range_start[c] = code_point   
+            range_start[c] = code_point
     prev = code_point
 
 for c in classes:

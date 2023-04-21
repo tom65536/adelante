450848414e6af798eb577df92a03252b3ad71cd2
Thomas Reiter <peregrinus@planet.ms>
2023-04-21 09:52:29 +0000

    adds

--- adelante-compiler-frontend/src/test/java/com/github/tom65536/adelante/parser/UnicodeReaderTest.java
+++ adelante-compiler-frontend/src/test/java/com/github/tom65536/adelante/parser/UnicodeReaderTest.java
@@ -1,5 +1,8 @@
 package com.github.tom65536.adelante.parser;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.stream.Stream;
@@ -13,6 +16,10 @@ public class UnicodeReaderTest {
     void testUtf8WithBom() {
        final byte[] sent = SENTENCE.getBytes(StandardCharsets.UTF_8);
        final byte[] bom = ByteOrderMark.UTF_8.getBytes();
-       //final byte[] bytes = ArrayUtils.addAll(bom, sent);
+       final InputStream in = new ByteArrayInputStream(
+            ByteBuffer.allocate(bom.length+sent.length)
+            .put(bom)
+            .put(sent, bom.length, sent.length)
+            .array());
     }
 }

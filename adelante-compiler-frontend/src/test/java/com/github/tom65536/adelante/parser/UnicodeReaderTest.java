8fec5e1831d08997010c6bbbdde9b9c588fbd011
Thomas Reiter <peregrinus@planet.ms>
2023-04-21 13:05:31 +0000

    nearly fixes test

--- adelante-compiler-frontend/src/test/java/com/github/tom65536/adelante/parser/UnicodeReaderTest.java
+++ adelante-compiler-frontend/src/test/java/com/github/tom65536/adelante/parser/UnicodeReaderTest.java
@@ -1,25 +1,45 @@
 package com.github.tom65536.adelante.parser;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.Iterator;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 import org.apache.commons.io.ByteOrderMark;
+import org.apache.commons.io.IOUtils;
+
 import org.testng.annotations.*;
+import static org.testng.Assert.*;
 
 public class UnicodeReaderTest {
     public static final String SENTENCE = "Hello world, Καλημέρα κόσμε, コンニチハ";
-    @Test
-    void testUtf8WithBom() {
-       final byte[] sent = SENTENCE.getBytes(StandardCharsets.UTF_8);
-       final byte[] bom = ByteOrderMark.UTF_8.getBytes();
-       final InputStream in = new ByteArrayInputStream(
-            ByteBuffer.allocate(bom.length+sent.length)
-            .put(bom)
-            .put(sent, bom.length, sent.length)
-            .array());
+
+    @Test(dataProvider = "bom-provider")
+    void testBoms(ByteOrderMark bom, Charset chs) throws IOException {
+        final byte[] sent = SENTENCE.getBytes(chs);
+        final ByteBuffer buffer = ByteBuffer.allocate(((bom==null)?0:bom.length())+sent.length);
+        if(bom!=null) buffer.put(bom.getBytes());
+        buffer.put(sent);
+
+        final InputStream in = new ByteArrayInputStream(buffer.array());
+        final UnicodeReader reader = new UnicodeReader(in);
+        final StringWriter writer = new StringWriter();
+        IOUtils.copy(reader, writer);
+        assertEquals(writer.toString(), SENTENCE);
+    }
+
+    @DataProvider(name = "bom-provider")
+    Iterator<Object[]> createBOM() {
+        return Arrays.asList(
+            new Object[]{ByteOrderMark.UTF_8, StandardCharsets.UTF_8},
+            new Object[]{ByteOrderMark.UTF_16BE, StandardCharsets.UTF_16BE},
+            new Object[]{ByteOrderMark.UTF_16LE, StandardCharsets.UTF_16LE},
+            new Object[]{null, StandardCharsets.UTF_8}
+        ).iterator();
     }
 }

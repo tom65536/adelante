eafc762cbb65c57f384d6163b1782d385820fec9
Thomas Reiter <peregrinus@planet.ms>
2023-04-22 19:32:20 +0000

    [NOCI]

--- adelante-compiler-frontend/src/test/java/com/github/tom65536/adelante/parser/UnicodeReaderTest.java
+++ adelante-compiler-frontend/src/test/java/com/github/tom65536/adelante/parser/UnicodeReaderTest.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 
 import org.apache.commons.io.ByteOrderMark;
 import org.apache.commons.io.IOUtils;
-
+import org.apache.commons.io.input.BOMInputStream;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 
@@ -20,26 +20,29 @@ public class UnicodeReaderTest {
     public static final String SENTENCE = "Hello world, Καλημέρα κόσμε, コンニチハ";
 
     @Test(dataProvider = "bom-provider")
-    void testBoms(ByteOrderMark bom) throws IOException {
-        final Charset chs = (bom == null)? StandardCharsets.UTF_8 : Charset.forName(bom.getCharsetName());
-        final int boml = (bom==null)? 0 : bom.length();
-        final byte[] sent = SENTENCE.getBytes(chs);
-        final byte[] raw = new byte[sent.length+boml];
-        System.arraycopy(bom.getBytes(), 0, raw, 0, boml);
-        System.arraycopy(sent, 0, raw, bom.length(), sent.length);
-
-        final InputStream in = new ByteArrayInputStream(raw);
+    void testBoms(final ByteOrderMark bom) throws IOException {
+        final InputStream in= encodeSentence(bom);
         final UnicodeReader reader = new UnicodeReader(in);
-
-        assertEquals(chs, reader.getCharset());
         
         final StringWriter writer = new StringWriter();
         IOUtils.copy(reader, writer);
         assertEquals(writer.toString(), SENTENCE);
     }
 
-    private byte[] encodeSentence(final ByteOrderMark bom) {
+    @Test(dataProvider = "bom-provider")
+    void testBOMInputStream(final ByteOrderMark bom) throws IOException {
+        final BOMInputStream in = new BOMInputStream(encodeSentence(bom));
+        assertEquals(in.getBOM(), bom);
+    }
+
+    private InputStream encodeSentence(final ByteOrderMark bom) {
         final Charset chs = (bom == null)? StandardCharsets.UTF_8 : Charset.forName(bom.getCharsetName());
+        final int boml = (bom == null)? 0 : bom.length();
+        final byte[] sent = SENTENCE.getBytes(chs);
+        final byte[] raw = new byte[sent.length+boml];
+        System.arraycopy(bom.getBytes(), 0, raw, 0, boml);
+        System.arraycopy(sent, 0, raw, bom.length(), sent.length);
+        return new ByteArrayInputStream(raw);
     }
 
     @DataProvider(name = "bom-provider")

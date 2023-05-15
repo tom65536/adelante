e5a940d60364b0e72ae2c8bbd1ad51846bb4037c
Thomas Reiter <peregrinus@planet.ms>
2023-05-15 04:01:13 +0000

    WIP implement symbol table

--- adelante-compiler-frontend/src/main/java/com/github/tom65536/adelante/parser/SymbolTable.java
+++ adelante-compiler-frontend/src/main/java/com/github/tom65536/adelante/parser/SymbolTable.java
@@ -4,16 +4,22 @@ package com.github.tom65536.adelante.symboltable;
 import com.github.tom65536.adelante.parser.Token;
 
 /**
- * Symbol rable implementation.
+ * Symbol table implementation.
  */
 public class SymbolTable {
+
     /**
      * Entry in a symbol table.
      */
     public static class Entry {
         /**
-         * Initialize a new entry,
-         * 
+         * The token at the defining code position.
+         */
+        private final Token token;
+
+        /**
+         * Initialize a new entry.
+         *
          * @param aToken the defining token.
          */
         public Entry(final Token aToken) {
@@ -22,13 +28,27 @@ public class SymbolTable {
     }
 
     /**
+     * Parent scope.
+     */
+    private final transient SymbolTable parent;
+
+    /**
      * Initialize a new symbol table.
-     * 
+     *
+     * The symbol table is initialized with
+     * no parent.
      */
     public SymbolTable() {
-
+        this(null);
     }
 
+    /**
+     * Initialize a new symbol table.
+     *
+     * @param aParent the parent scope.
+     */
+    public SymbolTable(final SymbolTable aParent) {
+        this.parent = aParent;
     }
 
 }
